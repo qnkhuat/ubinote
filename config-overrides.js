f75eff9b08225c208386974fe6902faa76858a65
Ngoc Khuat <qn.khuat@gmail.com>
2022-06-13 13:29:35 +0700

    serving FE using BE server

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 module.exports = {
-    paths: function (paths, env) {
-        paths.appIndexJs = path.resolve(__dirname, 'src/frontend/index.tsx');
-        paths.appSrc = path.resolve(__dirname, 'src/frontend');
-        return paths;
-    },
+  // during dev write the build to disk so that BE can serves it
+  devServer: function(configFunction) {
+    return function(proxy, allowedHost) {
+      const config = configFunction(proxy, allowedHost);
+      config.devMiddleware['writeToDisk'] = true;
+      return config;
+    }
+  },
+  paths: function (paths, _env) {
+    paths.appIndexJs = path.resolve(__dirname, 'src/frontend/index.tsx');
+    paths.appSrc = path.resolve(__dirname, 'src/frontend');
+    // build into the resources folder of java code app
+    paths.appBuild = path.join(__dirname, 'resources/frontend');
+    return paths;
+  },
 }
--- src/ubinote/api/core.clj
+++ src/ubinote/api/core.clj
@@ -5,12 +5,15 @@
             [ubinote.api.comment :as cmt]
             [ubinote.api.user :as user]
             [ubinote.api.session :as session]
+            [ubinote.server.middleware.core :as middleware]
             [ubinote.server.middleware.auth :refer [+auth]]))
 
-(defroutes routes
+(defroutes ^:private api-routes
   (GET "/health" []         "Doing great!")
   (context "/page" []       (+auth page/routes))
   (context "/user" []       (+auth user/routes))
   (context "/annotation" [] (+auth ant/routes))
   (context "/comment" []    (+auth cmt/routes))
   (context "/session" []    session/routes))
+
+(def routes (middleware/apply-middleware api-routes middleware/middlewares))
