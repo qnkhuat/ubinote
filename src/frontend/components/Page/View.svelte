450d2b68038c2076879b0bac8750f9516a1f4c5f
Ngoc Khuat <qn.khuat@gmail.com>
2023-01-06 11:44:38 +0700

    Divide things into components

---
+++ src/frontend/components/Page/View.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { onMount } from "svelte";
+	import * as api from "frontend/api.js";
+
+	// props
+	export let pageId;
+
+	// state
+	let pageDetail;
+	let pageContent;
+
+	onMount(() => {
+		api.getPage(pageId)
+			.then(resp =>pageDetail = resp.data);
+
+		api.getPageContent(pageId)
+			.then(resp => pageContent = resp.data);
+	});
+
+</script>
+
+{#if pageContent}
+<div id="page-content">
+	{@html pageContent}
+</div>
+{:else}
+<div>Loading...</div>
+{/if}
+
+<style>
+	#page-content {
+		width: 100%;
+		positiion: relative;
+	}
+</style>
--- src/frontend/routes/page/[id].svelte
+++ src/frontend/routes/page/[id].svelte
@@ -1,37 +1,13 @@
 <script>
 	import { onMount } from "svelte";
 	import * as api from "frontend/api.js";
+	import PageView from "frontend/components/Page/View.svelte";
+
 	export let currentRoute;
-	let pageDetail;
-	let pageContent;
 	const pageId = currentRoute.namedParams.id;
-	console.log(pageDetail);
-
-	onMount(() => {
-		api.getPage(pageId)
-			.then(resp =>pageDetail = resp.data);
-
-		api.getPageContent(pageId)
-			.then(resp => pageContent = resp.data);
-
-	});
-
 </script>
 
 <div id="page-view">
-	{#if pageContent}
-		<div id="page-content">
-			{@html pageContent}
-		</div>
-	{:else}
-		<div>Loading...</div>
-	{/if}
+	<PageView {pageId} />
 </div>
 
-<style>
-	#page-content {
-		width: 100%;
-		positiion: relative;
-	}
-
-</style>
