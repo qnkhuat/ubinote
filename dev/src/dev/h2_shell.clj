2a1ca9334e9b268274254242d1cc7233ae3d379f
Ngoc Khuat <qn.khuat@gmail.com>
2023-03-06 12:31:17 +0700

    h2-shell

---
+++ dev/src/dev/h2_shell.clj
@@ -0,0 +1,17 @@
+(ns dev.h2-shell
+  (:require
+    [ubinote.config :as cfg]
+    [ubinote.server.db :as db]))
+
+(defn shell
+  "Open an H2 shell with `clojure -X:h2`."
+  [& _args]
+  ;; Force the DB to use h2 regardless of what's actually in the env vars for Java properties
+  (alter-var-root #'cfg/env assoc :un-db-type "h2")
+  (require 'ubinote.config :reload)
+  (org.h2.tools.Shell/main
+    (into-array
+      String
+      ["-url" (let [url(.getJdbcUrl (:datasource db/*application-db*))]
+                (println "Connecting to database at URL" url)
+                url)])))
