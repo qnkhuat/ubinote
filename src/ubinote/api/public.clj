2056d686db433bb2d02d3145210174256971d74c
Ngoc Khuat <qn.khuat@gmail.com>
2023-04-20 22:11:41 +0700

    remove debug code

--- src/ubinote/api/public.clj
+++ src/ubinote/api/public.clj
@@ -1,18 +1,14 @@
 (ns ubinote.api.public
   (:require
-    [compojure.coercions :refer [as-int]]
-    [compojure.core :refer [context defroutes DELETE POST GET]]
-    [malli.core :as mc]
+    [compojure.core :refer [context defroutes GET]]
     [ring.util.response :as response]
     [toucan2.core :as tc]
     [ubinote.api.common :as api]
     [ubinote.api.page]
-    [ubinote.models.common.schema :as schema]
     [ubinote.models.page :as page]))
 
 (defn get-public-page
   [uuid _req]
-  (println "GETTING PUBLIC PAGE")
   (-> (api/check-404 (tc/select-one :m/page :public_uuid uuid))
       (tc/hydrate :annotations)))
 
