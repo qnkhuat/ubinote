d90938151bc985c88e13113bf74b0c37f06b6fb7
Ngoc Khuat <qn.khuat@gmail.com>
2023-04-21 10:00:04 +0700

    fixes failed to start jar

--- src/ubinote/server/routes.clj
+++ src/ubinote/server/routes.clj
@@ -10,8 +10,7 @@
   (context "/api" [] api/routes)
   (GET "/build/bundle.js" [_req] (resource-response "frontend/build/bundle.js"))
   (GET "/build/bundle.css" [_req] (resource-response "frontend/build/bundle.css"))
-  (when cfg/is-dev?
-   (GET "/build/bundle.js.map" [_req] (resource-response "frontend/build/bundle.js.map"))
-   (GET "/build/bundle.css.map" [_req] (resource-response "frontend/build/bundle.css.map")))
+  #_(GET "/build/bundle.js.map" [_req] (resource-response "frontend/build/bundle.js.map"))
+  #_(GET "/build/bundle.css.map" [_req] (resource-response "frontend/build/bundle.css.map"))
   ;; let svelte handles it from here
   (GET "*" [] (resource-response "frontend/index.html")))
